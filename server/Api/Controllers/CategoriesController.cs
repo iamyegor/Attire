08d3703b8b218b6b21fd370e90b25bc867b9be22
Den4ik-dev <ignatenko.de2017@yandex.ru>
2024-08-07 15:37:15 +0300

    add GetFemaleCategoriesQuery

--- server/Api/Controllers/CategoriesController.cs
+++ server/Api/Controllers/CategoriesController.cs
@@ -1,6 +1,28 @@
-﻿namespace Api.Controllers;
+﻿using Api.Controllers.Common;
+using Application.Categories.Queries.Common;
+using Application.Categories.Queries.GetFemaleCategories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
-public class CategoriesController
+namespace Api.Controllers;
+
+[Route("api/categories")]
+public class CategoriesController : ApplicationController
 {
-    
+    private readonly ISender _sender;
+
+    public CategoriesController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("female")]
+    public async Task<IEnumerable<CategoryShortDto>> GetFemaleCategories()
+    {
+        var query = new GetFemaleCategoriesQuery();
+
+        IEnumerable<CategoryShortDto> result = await _sender.Send(query);
+
+        return result;
+    }
 }
