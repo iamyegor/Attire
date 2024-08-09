9ea88c4795d3bbd6b8ac205782cb3d62af4b989d
yegor <astery227@gmail.com>
2024-08-09 22:18:21 +0300

    fix: fix skeleton margins on the catalog page

--- client/src/pages/CatalogPage/components/Products.tsx
+++ client/src/pages/CatalogPage/components/Products.tsx
@@ -18,16 +18,18 @@ export default function Products() {
 
     function skeleton() {
         return Array.from({ length: 8 }).map((_, index) => (
-            <BaseSkeleton
-                key={index}
-                style={{ width: "100%", aspectRatio: "10/16", flexShrink: 0 }}
-            ></BaseSkeleton>
+            <div className="w-full flex-shrink-0 p-4">
+                <BaseSkeleton
+                    key={index}
+                    style={{ width: "100%", aspectRatio: "10/16" }}
+                ></BaseSkeleton>
+            </div>
         ));
     }
 
     return (
         <div
-            className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center w-full ${isLoading ? "gap-4" : ""}`}
+            className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center w-full`}
         >
             {isLoading && skeleton()}
             {products.map((product) => (
