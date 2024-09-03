8e73474ca08eb3ce212572ea839ef5b81ab1a87b
yegor <astery227@gmail.com>
2024-09-03 15:12:12 +0300

    feat: center "no products in category" text

--- client/src/pages/CatalogPage/CatalogPage.tsx
+++ client/src/pages/CatalogPage/CatalogPage.tsx
@@ -18,16 +18,16 @@ export default function CatalogPage() {
     }
 
     return (
-        <div className="mt-4 pb-8 space-y-3 lg:mt-0">
+        <div className="mt-4 pb-8 space-y-3 lg:mt-0 h-full">
             <Breadcrumbs type={type} category={category} />
-            <div className="flex mb-8 w-full">
+            <div className="flex mb-8 w-full h-full">
                 <div className="lg:px-4 relative">
                     <CategoriesDashboard
                         currentType={type}
                         currentCategoryId={category?.id ?? null}
                     />
                 </div>
-                <div className="flex flex-col space-y-3 w-full">
+                <div className="flex flex-col space-y-3 w-full h-full">
                     <div className="ml-4 flex space-x-5 h-11">
                         <SortByComponent />
                         {category && !isNewCategory() && <FilterComponent category={category} />}
